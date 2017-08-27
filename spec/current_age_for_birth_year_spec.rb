988ef63398352120b3bc90683ac8a4caaa6ab44b
Darren Kuo <wasd31627@gmail.com>
2017-08-27 10:50:26 +0000

    automatically backed up by learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -8,6 +8,6 @@ describe "current_age_for_birth_year method" do
   end
 end
 
-def current_age_for_birth_year(x)
-  return answer=2003-x
+def current_age_for_birth_year(birth_year)
+  2003 - birth_year
 end
